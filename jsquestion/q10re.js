752f8e61d5b6d14c45eb4667245b4d854644ceae
siromiji <mnmn82018282@gmail.com>
2025-08-26 00:46:18 +0900

    250828 10문제 째

---
+++ jsquestion/q10re.js
@@ -0,0 +1,19 @@
+// for (let i =1;i<=5;i++){
+//     let line = "";
+//     for(let j =1; j<=i;j++){
+//         line += "*";
+//     }
+//     console.log(line);
+// }
+
+// for (let i = 1;i <=5; i++){
+//     let line ="";
+//     for (let a = 1; a<=5-i;a++){
+//         line += " ";
+//     }
+//     for (let b = 1; b <=i;b++){
+//         line += "*";
+//     }
+//     console.log(line);
+// }
+
